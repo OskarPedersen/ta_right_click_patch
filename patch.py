e5405965c066c45564a46f5173ba3956508e8ec0
Oskar Pedersen <oskar.pedersen1@gmail.com>
2023-04-02 18:40:41 +0200

    Simple script for applying the patch

---
+++ patch.py
@@ -0,0 +1,13 @@
+with open('TotalA.exe', 'rb') as source_file:
+    file_content = source_file.read()
+
+    # Make the check for right click interface always fail when getting the cursor type
+    # So that it returns the same cursor as when left click interface is used
+    file_content = file_content.replace(b'\x83\xBB\xFA\x7E\x03\x00\x01', b'\x83\xBB\xFA\x7E\x03\x00\xFF')
+
+    # Change the function that handles left click so that checks for right/left interface before checking the current cursor type
+    file_content = file_content.replace(b'\x80\xfa\x11\x7c\x27\x83\xb8\xfa\x7e\x03\x00\x01\x0f\x85\xa3\x00\x00\x00\x80\xf9\x01\x0f\x85\x9a\x00\x00\x00\xe8\x9f\x2c\xff\xff\x6a\x01\xe8\x08\x8d\xff\xff\x5e\x59\xc2\x04\x00', b'\x83\xb8\xfa\x7e\x03\x00\x01\x0f\x85\x1a\x00\x00\x00\x80\xf9\x01\x0f\x85\x16\x00\x00\x00\xe8\xa4\x2c\xff\xff\x6a\x01\xe8\x0d\x8d\xff\xff\x5e\x59\xc2\x04\x00\x80\xfa\x11\x7d\xf6')
+
+with open('TotalA-right-click-patch.exe', 'wb') as output_file:
+    output_file.write(file_content)
+
